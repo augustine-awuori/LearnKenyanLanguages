b15b592ba2f01190ea0e4b28280371bcc5387718
Augustine Awuori <augustineawuori95@gmail.com>
2022-06-22 13:41:07 +0300

    Improve the look and feel of the navigation for mobile.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 import cancel from "../assets/icons/cancel.svg";
 import logo from "../assets/logo.jpg";
@@ -18,7 +19,7 @@ export default function NavBar() {
     setCollapsibleClassName(collapsibleClassName + getNavItemsName());
   }, [isExpanded]);
 
-  const handleClick = () => setIsExpanded(!isExpanded);
+  const switchExpansion = () => setIsExpanded(!isExpanded);
 
   return (
     <nav className="nav collapsible">
@@ -27,22 +28,33 @@ export default function NavBar() {
         <img
           src={cancel}
           className="icon nav__close-icon"
-          alt="logo"
-          onClick={handleClick}
+          alt="icon"
+          onClick={switchExpansion}
         />
       ) : (
         <img
           src={menu}
           className="icon nav__close-icon"
-          alt="logo"
-          onClick={handleClick}
+          alt="icon"
+          onClick={switchExpansion}
         />
       )}
       <ul className={collapsibleClassName}>
-        <li className="nav__item">Partner With Us</li>
-        <li className="nav__item">Contact Us</li>
-        <li className="nav__item">Sign In</li>
-        <li className="nav__item">Sign Up</li>
+        <NavLink to="/partner-with-us" onClick={switchExpansion}>
+          <li className="nav__item">Partner With Us</li>
+        </NavLink>
+        <NavLink to="/contact-us" onClick={switchExpansion}>
+          <li className="nav__item">Contact Us</li>
+        </NavLink>
+        <NavLink to="/login" onClick={switchExpansion}>
+          <li className="nav__item">Sign In</li>
+        </NavLink>
+        <NavLink to="/register" onClick={switchExpansion}>
+          <li className="nav__item">Sign Up</li>
+        </NavLink>
+        <NavLink to="/log-out" onClick={switchExpansion}>
+          <li className="nav__item">Sign Out</li>
+        </NavLink>
       </ul>
     </nav>
   );
