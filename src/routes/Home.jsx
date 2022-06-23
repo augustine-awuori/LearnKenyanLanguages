f340d598ef6b4900bbb4c4320e6e29a801e73d7f
Augustine Awuori <augustineawuori95@gmail.com>
2022-06-23 15:15:27 +0300

    Create a hero component.

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
+import logo from "../assets/logo.jpg";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <main className="hero grid grid--1x2">
+      <article>
+        <h1 className="hero__heading">Learn Kenyan Languages With Ease</h1>
+        <p className="hero__tagline">
+          We teach various Kenyan languages including local and national. We
+          teach languages such as Kikuyu, Swahili and Luhya
+        </p>
+      </article>
+      <img className="hero__logo" src={logo} alt="logo" />
+    </main>
+  );
 }
