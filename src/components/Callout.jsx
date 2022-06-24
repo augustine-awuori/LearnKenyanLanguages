9e8e65e986a56c363eb68b0ee564a3e77321f698
Augustine Awuori <augustineawuori95@gmail.com>
2022-06-24 11:23:15 +0300

    Change order of callout images.

--- src/components/Callout.jsx
+++ src/components/Callout.jsx
@@ -1,21 +1,15 @@
 import React from "react";
 
 import CalloutButton from "./CalloutButton";
-import logo from "../assets/logo.jpg";
 
-export default function Callout({ heading, content }) {
+export default function Callout({ image, heading, content, targetPageUrl }) {
   return (
     <section className="block callout grid grid--1x2">
-      <img className="grid__image" src={logo} alt="" />
+      <img className="grid__image callout__image" src={image} alt="" />
       <article>
-        <h2 className="block__heading callout__heading">Callout</h2>
-        <p className="block__content callout__content">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
-          tempore. Iusto earum doloribus quod exercitationem veniam voluptates
-          voluptatibus similique nobis sapiente quia alias quaerat, eveniet
-          officia animi sunt numquam nostrum?
-        </p>
-        <CalloutButton />
+        <h2 className="block__heading callout__heading">{heading}</h2>
+        <p className="block__content callout__content">{content}</p>
+        <CalloutButton to={targetPageUrl} />
       </article>
     </section>
   );
