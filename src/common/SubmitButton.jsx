51f76e6617ba5723fe36461677c4bef6db72dc4d
Augustine Awuori <augustineawuori95@gmail.com>
2022-07-17 21:24:58 +0300

    Remove unused variables.

--- src/common/SubmitButton.jsx
+++ src/common/SubmitButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useFormikContext } from "formik";
 
 export default function SubmitButton({ label }) {
-  const { errors, handleSubmit } = useFormikContext();
+  const { handleSubmit } = useFormikContext();
   // Don't remove the destructured 'errors' even though it's not used.
   // Currently you cannot destructure only one 'object'  or 'function;
   return (
