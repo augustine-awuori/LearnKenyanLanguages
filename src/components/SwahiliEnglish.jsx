f4c176acfcf4d9d5e8f582474716e90b36261e0b
Augustine Awuori <codewithaugustine@gmail.com>
2022-07-31 21:30:18 +0300

    Improve the look and feel.

--- src/components/SwahiliEnglish.jsx
+++ src/components/SwahiliEnglish.jsx
@@ -1,22 +1,32 @@
 import React from "react";
 
 import useChevron from "../hooks/useChevron";
+import icon from "../assets/icons/chevron-right.svg";
 
 export default function SwahiliEnglish({ english, swahili }) {
-  const { chevron, isExpanded, toggleExpansion } = useChevron();
+  const { isExpanded, toggleExpansion } = useChevron();
+
+  const getClassName = () => {
+    let name = "swahili-english ";
+    return isExpanded ? (name += "collapsible--expanded") : name;
+  };
 
   return (
-    <article className="swahili-english">
+    <article className={getClassName()}>
       <section
         className="swahili-english__swahili-container"
         onClick={toggleExpansion}
       >
         <p className="swahili-english__swahili">{swahili}</p>
-        <img src={chevron} alt="chevron icon" className="icon" />
+        <img
+          src={icon}
+          alt="chevron icon"
+          className="icon collapsible__chevron"
+        />
       </section>
-      {isExpanded && (
+      <article className="collapsible__content">
         <p className="translation swahili-english__english">{english}</p>
-      )}
+      </article>
     </article>
   );
 }
